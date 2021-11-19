31a66c215658e46516f85a864f691f20f52b69d3
kartiksharma548 <kartiksharma548@gmail.com>
2021-11-19 23:15:19 +0530

    Auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ShowcaseComponent } from './component/showcase/showcase.component';
 import { HomeComponent } from './component/home/home.component';
+import { AuthGuard } from './services/auth.guard';
 
 
 const routes: Routes = [
   {
     path : '',
-    component :HomeComponent
+    component :HomeComponent,
+    canActivate:[AuthGuard]
   }
 ];
 @NgModule({
