75317f7cc8e6f2bc29d012fc596ed487d673cfec
puneetxp <puneetsharma9@hotmail.com>
2021-11-26 01:44:13 +0530

    0.0.7.5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { AuthGuard } from './services/auth.guard';
+import { CartComponent } from './user/cart/cart.component';
 import { LoginComponent } from './user/login/login.component';
 import { RegisterComponent } from './user/register/register.component';
 
@@ -21,6 +22,11 @@ const routes: Routes = [
   {
     path: 'signup',
     component: RegisterComponent,
+    canActivate:[AuthGuard]
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
     canActivate:[AuthGuard]
   }
 ];
