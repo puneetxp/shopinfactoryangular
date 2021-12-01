d89fb4850812dccfc9b56b1dcebb3c8eddccd395
puneetxp <puneetsharma9@hotmail.com>
2021-12-01 19:59:58 +0530

    0.0.7.6

--- src/app/user/cart/cart.component.ts
+++ src/app/user/cart/cart.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartComponent implements OnInit {
 
+  items: Array<any> = [
+    {
+      product_name: 'first',
+      photo: 
+        {
+          src : 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+          alt : 'test123',
+        },
+      category : 'Case'
+    },
+    {
+      product_name: 'second',
+      photo: 
+      {
+        src : 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+        alt : 'test13',
+      },
+      category : 'Screen Guard'
+    }
+  ]
+  suggestion: Array<any> = [
+    {
+      product_name: 'any'
+    },
+    {
+      product_name: 'one'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
